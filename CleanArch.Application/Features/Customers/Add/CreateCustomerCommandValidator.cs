51e9aaf0e7d3c00c9bec3cda8dd662a9b43da382
doganbacaksiz <dogan.bacaksiz@gmail.com>
2025-03-05 22:42:28 +0300

    validation and mappig

--- CleanArch.Application/Features/Customers/Add/CreateCustomerCommandValidator.cs
+++ CleanArch.Application/Features/Customers/Add/CreateCustomerCommandValidator.cs
@@ -1,5 +1,12 @@
-﻿namespace CleanArch.Application.Features.Customers.Add;
+﻿using FluentValidation;
 
-public sealed class CreateCustomerCommandValidator
+namespace CleanArch.Application.Features.Customers.Add;
+
+public sealed class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
 {
+    public CreateCustomerCommandValidator()
+    {
+        RuleFor(p => p.taxnumber).NotEmpty().WithMessage("Tax number is required").MinimumLength(10)
+            .WithMessage("Tax number minimum 10 char").MaximumLength(11).WithMessage("Tax number maximum 11 char");
+    }
 }
---
+++ CleanArch.Application/Mapping/CustomerProfiles/CustomerMappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CleanArch.Application.Features.Customers.Add;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Application.Mapping.CustomerProfiles;
+
+public class CustomerMappingProfiles : Profile
+{
+    public CustomerMappingProfiles()
+    {
+        CreateMap<CreateCustomerCommand, Customer>();
+    }
+}
